ef1fa74f4b8b80e3a5888ea8d977e50fa6100953
zeyadsadek03-prog <zeyadsadek03@gmail.com>
2026-06-13 18:18:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-# app.py
-import streamlit as st
-from PIL import Image
+import gradio as gr
 import numpy as np
 import tensorflow as tf
 import cv2
+from PIL import Image
 
 model = tf.keras.models.load_model('model/handwritten.keras')
 
-st.title("Handwritten Digit Recognizer")
-uploaded = st.file_uploader("Upload a digit image", type=["png","jpg"])
-
-if uploaded:
-    img = Image.open(uploaded).convert('L')
-    img = np.array(img)
+def predict(image):
+    img = np.array(image.convert('L'))
     img = cv2.resize(img, (28, 28))
     img = cv2.bitwise_not(img)
     img = tf.keras.utils.normalize(img, axis=1)
     img = np.array([img])
-    
     prediction = model.predict(img)
-    st.write(f"## Predicted Digit: {np.argmax(prediction)}")
+    return f"Predicted Digit: {np.argmax(prediction)}"
+
+demo = gr.Interface(
+    fn=predict,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    title="Handwritten Digit Recognizer"
+)
+
+demo.launch()
